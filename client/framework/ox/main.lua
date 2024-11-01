a64f4cbb8b27c95835959ad631b337f2231f9c50
Filipe Junqueiro <144694602+filipejunqueiro@users.noreply.github.com>
2024-11-01 14:59:33 +0000

    feat(ox_core): added support for groups
    This was never tested on a running server, soo i cannot tell if it works or not, but i made my best while not being home.

--- client/framework/ox/main.lua
+++ client/framework/ox/main.lua
@@ -1,41 +1,47 @@
 if not Framework.Ox() then return end
 
+-- [ Local Variables ] --
+
 local Ox = require '@ox_core.lib.init'
 local player = Ox.GetPlayer()
 
-RegisterNetEvent("ox:setActiveCharacter", function(character)
-    if character.isNew then
-        return InitializeCharacter(Framework.GetGender(true))
-    end
+-- [ Local Functions ] --
 
-    InitAppearance()
-end)
-
----@todo groups
--- RegisterNetEvent("ox:setGroup", function(group, grade)
--- end)
-
-function Framework.GetPlayerGender()
-    return player.get('gender') == 'female' and 'Female' or 'Male'
+local function onSetActiveCharacter(character)
+    if not character.isNew then InitAppearance() return end
+    InitializeCharacter(Framework.GetGender(true))
 end
+
+local function getPlayerGroupGradeByGroupType(groupType)
+    local playerData = {charId = player.get('charId'), groups = player.getGroups(), groupType = groupType}
+    return lib.callback.await('illenium-appearance:server:ox:getGroupGradeByType', false, playerData)
+end
+
+-- [ Net Events ] --
+
+RegisterNetEvent('ox:setActiveCharacter', onSetActiveCharacter)
+
+-- [ Lib Client Callbacks ] --
+
+lib.callback.register('illenium-appearance:client:ox:resetBlips', ResetBlips)
+
+-- [ Global Functions ] --
+
+function Framework.GetPlayerGender() return player.get('gender') == 'female' and 'Female' or 'Male' end
 
 function Framework.UpdatePlayerData() end
 
 function Framework.HasTracker() return false end
 
-function Framework.CheckPlayerMeta()
-    return LocalPlayer.state.isDead or IsPedCuffed(cache.ped)
-end
+function Framework.CheckPlayerMeta() return LocalPlayer.state.isDead or IsPedCuffed(cache.ped) end
 
-function Framework.IsPlayerAllowed(charId)
-    return charId == player.charId
-end
+function Framework.IsPlayerAllowed(charId) return charId == player.charId end
 
-function Framework.GetRankInputValues() end
+function Framework.GetRankInputValues(groupType) return lib.callback.await('illenium-appearance:server:ox:getGroupGrades', false, groupType) end
 
-function Framework.GetJobGrade() end
+function Framework.GetJobGrade() return getPlayerGroupGradeByGroupType('job') end
 
-function Framework.GetGangGrade() end
+function Framework.GetGangGrade() return getPlayerGroupGradeByGroupType('gang') end
 
 function Framework.CachePed() end
 
--- server/framework/ox/main.lua
+++ server/framework/ox/main.lua
@@ -1,26 +1,94 @@
 if not Framework.Ox() then return end
 
+-- [ Local Variables ] --
+
 local Ox = require '@ox_core.lib.init'
 
-function Framework.GetPlayerID(playerId)
-    return Ox.GetPlayer(playerId).charId
+-- [ Local Functions ] --
+
+---@param grades table
+---@return table
+local function formatGroupGrades(grades)
+    local tempGrades = {}
+    for i = 1, #grades do tempGrades[#tempGrades + 1] = {label = grades[i].label, value = i} end
+    if not #tempGrades >= 1 then return {} end
+    return tempGrades
 end
 
-function Framework.HasMoney(playerId, item, amount)
-    return exports.ox_inventory:GetItemCount(playerId, item) >= amount
+---@param groupType string
+---@return table
+local function getGroupGrades(_, groupType)
+    local group = Ox.GetGroupsByType(groupType)
+    if not type(group) == 'table' then return {} end
+    return formatGroupGrades(group.grades)
 end
 
-function Framework.RemoveMoney(playerId, type, amount)
-    return exports.ox_inventory:RemoveItem(playerId, type, amount)
+---@class PlayerData
+---@field charId number
+---@field groups table
+---@field groupType string
+
+---@param playerData PlayerData
+---@return number
+local function getGroupGradeByType(_, playerData)
+    for i = 1, #playerData.groups do
+        local group = playerData.groups[i]
+        if group.type == playerData.groupType then
+            local row = MySQL.single.await('SELECT `grade`, `isActive` FROM `character_groups` WHERE `name` = ? AND `charId` = ?', {group.name, playerData.charId})
+            if row and row.isActive then return row.grade end
+        end
+    end
+    return 0
 end
 
-function Framework.GetJob()
-    return ---@todo
+---@param name string
+---@param grade number
+---@return table
+local function fixGroupTable(name, grade)
+    return {name = name, grade = grade}
 end
 
-function Framework.GetGang()
-    return ---@todo
+---@param playerId number
+---@param groupType string
+local function getGroupByType(playerId, groupType)
+    local player = Ox.GetPlayer(playerId)
+    local groups = player.getGroups()
+    for i = 1, #groups do
+        local group = groups[i]
+        if group.type == groupType then
+            local row = MySQL.single.await('SELECT `grade`, `isActive` FROM `character_groups` WHERE `name` = ? AND `charId` = ?', {group.name, player.charId})
+            if row and row.isActive then return fixGroupTable(group.name, row.grade) end
+        end
+    end
 end
+
+local function resetBlips(playerId) return lib.callback.await('illenium-appearance:client:ox:resetBlips', playerId) end
+
+local function getPlayerId(playerId) return Ox.GetPlayer(playerId).charId end
+
+-- [ Server Events ] --
+
+AddEventHandler('ox:setActiveGroup', resetBlips)
+
+AddEventHandler('ox:setGroup', resetBlips)
+
+-- [ Lib Server Callbacks ] --
+
+lib.callback.register('illenium-appearance:server:ox:getGroupGrades', getGroupGrades)
+
+lib.callback.register('illenium-appearance:server:ox:getGroupGradeByType', getGroupGradeByType)
+
+-- [ Global Functions ] --
+
+function Framework.GetPlayerID(playerId) return getPlayerId(playerId) end
+
+function Framework.HasMoney(playerId, item, amount) return exports.ox_inventory:GetItemCount(playerId, item) >= amount end
+
+function Framework.RemoveMoney(playerId, type, amount) return exports.ox_inventory:RemoveItem(playerId, type, amount) end
+
+function Framework.GetJob(playerId) return getGroupByType(playerId, 'job') end
+
+function Framework.GetGang(playerId) return getGroupByType(playerId, 'gang') end
 
 function Framework.SaveAppearance(appearance, charId)
     Database.PlayerSkins.UpdateActiveField(charId, 0)
@@ -30,7 +98,5 @@ end
 
 function Framework.GetAppearance(charId, model)
     local result = Database.PlayerSkins.GetByCitizenID(charId, model)
-    if result then
-        return json.decode(result)
-    end
+    if result then return json.decode(result) end
 end
